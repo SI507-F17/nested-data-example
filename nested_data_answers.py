f70e697328a7967ccc765f135456a17526c7351e
cbredernitz <chrisrbredernitz@gmail.com>
2017-10-25 10:18:38 -0400

    Added change to new branch file

--- nested_data_answers.py
+++ nested_data_answers.py
@@ -1,13 +1,13 @@
 # Import twitter_data.py in twitter_data in the current script
 # 	- Note the file name does not include the extension ('.py')
-#	- The * is a wildcard. It's telling python I want to import 
+#	- The * is a wildcard. It's telling python I want to import
 # 	  everything from the file twitter_data.py
 from twitter_data import *
 # What is the difference between the above statement and the following:
 # import wtitter_data # You will need to comment the import above and uncomment this one to experiment
 					  # HINT: the code below assumes we are using the first type of import: 'from twitter_data import *'
 
-
+# Look at this brand new comment! Neat!
 
 # Basic strategy:
 # ========================================================================================
@@ -31,25 +31,25 @@ from twitter_data import *
 
 
 # If it's a dictionary, what are its keys?
-# If it's a list, how many elements does it have?, and 
+# If it's a list, how many elements does it have?, and
 # what is the type of its first element?
 # ----------------------------------------------------------
 #print res.keys()
-#print type(res['statuses']) 
+#print type(res['statuses'])
 
 
-# Why the key "search_metadata" is not relevant 
+# Why the key "search_metadata" is not relevant
 # to our purposes?
 # ----------------------------------------------------------
 
 
 # What is the type associated to the key 'statuses'?
 # ----------------------------------------------------------
-#print type(res['statuses']) 
+#print type(res['statuses'])
 
 
 # If it's a dictionary, what type are its keys?
-# If it's a list, how many elements does it have?, and 
+# If it's a list, how many elements does it have?, and
 # what is the type of its first element?
 # ----------------------------------------------------------
 #print len(res['statuses']) # 3 elements
@@ -62,15 +62,15 @@ from twitter_data import *
 #	print type(res['statuses'][i])
 
 
-# If it's the first element its a dictionary, 
+# If it's the first element its a dictionary,
 # what are its keys?
-# If it's a list, how many elements does it have?, and 
+# If it's a list, how many elements does it have?, and
 # what is the type of its first element?
 # ----------------------------------------------------------
 # print res['statuses'][0].keys()
 # print type(res['statuses'][0].keys()[0])
 
-# If it's a dictionary, how many keys does 
+# If it's a dictionary, how many keys does
 # the first dictionary have?
 # ----------------------------------------------------------
 #print len(res['statuses'][0].keys())
@@ -84,7 +84,7 @@ from twitter_data import *
 #	print len(res['statuses'][i].keys())
 
 
-# Nested under 'statuses', there are dictionaries, 
+# Nested under 'statuses', there are dictionaries,
 # do they have a key that contains the username I am looking for?
 # If so, what is the type and value associated to it?
 # ----------------------------------------------------------
@@ -106,16 +106,16 @@ from twitter_data import *
 # associated to it?
 # ----------------------------------------------------------
 # 'name'
-#print type(res['statuses'][0]['user']['name']) 
+#print type(res['statuses'][0]['user']['name'])
 #print res['statuses'][0]['user']['name']
 
 
 # Since you know there are three tweets in the database
 # You also know there is an equal number of usernames
 # corresponding to the users who twitted
-# By answerin the questions above, you have already found 
+# By answerin the questions above, you have already found
 # the first name
-# Can you iterate through the twitter data, so that you 
+# Can you iterate through the twitter data, so that you
 # access the other three names with a single print statement?
 # ----------------------------------------------------------
 #for i in range(len(res['statuses'])):
